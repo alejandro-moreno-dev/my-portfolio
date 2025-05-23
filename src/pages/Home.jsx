15cb1e78995cf8e2a1801fd47e47c8dffa26e821
Alejandro Moreno <162651273+alejandro-moreno-dev@users.noreply.github.com>
2025-05-23 15:45:47 -0600

    dumb spelling fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <section id="home" className="p-8">
-      <h2 className="text-3xl font-bold mb-4">Hi, Alex 👋</h2>
+      <h2 className="text-3xl font-bold mb-4">Hi, I'm Alex 👋</h2>
       <p className="text-white mb-4">I’m a front-end developer trying out new technologies and testing some stuff on my PC. </p>
-       <p className="text-white mb-4">This site is for <strong>testing only</strong>, but my personal blog will be available soon! In the mean time, why don't you try a cool aplication? It is just down below this boring text. <br /><strong>⬇️ ⬇️ ⬇️ TRY IT FOR FREE!⬇️ ⬇️ ⬇️ </strong></p>
+       <p className="text-white mb-4">This site is for <strong>testing only</strong>, but my personal blog will be available soon! In the meantime, why don't you try a cool application? It is just down below this boring text. <br /><strong>⬇️ ⬇️ ⬇️ TRY IT FOR FREE!⬇️ ⬇️ ⬇️ </strong></p>
       <hr />
     </section>
   );
